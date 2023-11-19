4d0943e365a087ff4dbcc1deeae65690e4c74e4d
wisskirchenj <juergen.wisskirchen@cofinpro.de>
2023-11-19 17:24:49 +0100

    Final stage completed.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const fs = require('node:fs');
 const { createHash } = require('node:crypto');
 
-extractUser = line => {
+const extractUser = line => {
   const lineArray = line.split(/\s*,\s*/);
   const user = {
     id: lineArray[0],
@@ -16,26 +16,36 @@ extractUser = line => {
   return user;
 };
 
-readCsv = () => {
+const readCsv = () => {
   const csv = fs.readFileSync('./database.csv', 'utf-8');
   const lines = csv.split('\n');
   const users = lines.slice(1).map(extractUser);
   return {
-    headers: lines[0], 
+    headers: lines[0],
     users: users
   };
 };
 
-writeHashes = (headers, users) => {
-  fs.writeFileSync('./hash_database.csv', headers);
+const writeWithHashes = (headers, users) => {
+  fs.writeFileSync('./filtered_database.csv', headers);
   users.forEach(user => {
-    fs.appendFileSync('./hash_database.csv', `\n${user.id}, ${user.name}, ${user.hash()}, ${user.consent}`);
+    fs.appendFileSync('./filtered_database.csv', `\n${user.id}, ${user.name}, ${user.hash()}, ${user.consent}`);
   });
 }
 
-const { headers, users } = readCsv();
-writeHashes(headers, users);
+const cleanData = users => {
+  return users.filter(user => user.consent !== '-' && user.name !== '-')
+    .map((user, id) => {
+      user.id = `${id + 1}`;
+      return user;
+    });
+}
+
+let { headers, users } = readCsv();
+users = cleanData(users);
+writeWithHashes(headers, users);
 
 exports.extractUser = extractUser;
 exports.readCsv = readCsv;
-exports.writeHashes = writeHashes;
+exports.writeHashes = writeWithHashes;
+exports.cleanData = cleanData;
--- main.test.js
+++ main.test.js
@@ -1,4 +1,4 @@
-const { extractUser, readCsv } = require('./main');
+const { extractUser, readCsv, cleanData } = require('./main');
 
 test('extractUser correctly extracts user and hashes password', () => {
   const line = '1, John Doe, password123, true';
@@ -11,7 +11,7 @@ test('extractUser correctly extracts user and hashes password', () => {
   expect(extractUser(line)).toMatchObject(expectedUser); // toMatchObject checks if the object has all the properties and values of the expected object
 });
 
-const expectedUser = {
+const firstUser = {
   id: '1',
   name: 'Mgkfuaqyw',
   password: 'v3iohwet',
@@ -21,6 +21,14 @@ const expectedUser = {
 test('readUsers correctly reads users from database.csv', () => {
   const { users } = readCsv();
   expect(users.length).toBe(100);
-  expect(users[0]).toMatchObject(expectedUser);
+  expect(users[0]).toMatchObject(firstUser);
   expect(users[0].hash()).toBe('8f9e2d2168b75ca7bdbb46021bf6a234b295d4cd6d8edcc6f747ce146ee69f2a')
+});
+
+test('cleanData correctly filters out users with no consent or name', () => {
+  const { users } = readCsv();
+  const cleanedUsers = cleanData(users);
+  expect(cleanedUsers.length).toBe(29);
+  expect(cleanedUsers[0]).toMatchObject(firstUser);
+  expect(cleanedUsers[0].hash()).toBe('8f9e2d2168b75ca7bdbb46021bf6a234b295d4cd6d8edcc6f747ce146ee69f2a')
 });
